3248bdb5d631e29dd556929743f20360cdd4cecb
LauraSpy <137756546+LauraSpy@users.noreply.github.com>
2024-07-15 16:52:22 +0200

    création des routes sur app.jsx (+ correction de la faute sur profil)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from './pages/homepage';
+import DetailFilm from './pages/detail_film';
+import UserProfile from './pages/user_profile';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/detail_film" element={<DetailFilm />} />
+          <Route path="/user_profile" element={<UserProfile />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
