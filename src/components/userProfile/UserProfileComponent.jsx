9f65a10e76dd672b1f5517bc9c1c1b79f840b1fc
AurelieTorre <aurelie.torre@laposte.net>
2024-07-17 14:59:41 +0200

    toggle plus joli

--- src/components/userProfile/UserProfileComponent.jsx
+++ src/components/userProfile/UserProfileComponent.jsx
@@ -30,23 +30,23 @@ const UserProfile = () => {
       <div className={s.profile_header}>
         <img src={user.avatar} alt="Avatar" className={s.avatar} />
         <div className={s.profile_header_text}>
-          <h1>{user.username}</h1>
+          <h1 id={s.name}>{user.username}</h1>
           <button onClick={handleProfileUpdate}>Modifier mon profil</button>
         </div>
       </div>
       <div className={s.profile_preferences}>
         <h2 id={s.pref}>Vos préférences de lecture</h2>
         <div className={s.preference}>
-          <label>Pays</label>
-          <select name="country" value={user.country} onChange={handleInputChange}>
+          <label>Pays : </label>
+          <select id={s.country} name="country" value={user.country} onChange={handleInputChange}>
             <option value="USA">USA</option>
             <option value="France">France</option>
             <option value="Germany">Germany</option>
           </select>
         </div>
         <div className={s.preference}>
-          <label>Langue</label>
-          <select name="language" value={user.language} onChange={handleInputChange}>
+          <label>Langue : </label>
+          <select id={s.lang} name="language" value={user.language} onChange={handleInputChange}>
             <option value="English">English</option>
             <option value="Français">Français</option>
             <option value="Deutsch">Deutsch</option>
