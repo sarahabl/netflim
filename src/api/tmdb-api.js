95a65e3c687274586363102bcc221760978c633c
sarahabl <sarahablonet.studio@gmail.com>
2024-07-22 15:56:58 +0200

    🔄 j'appelle plus de films

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -18,7 +18,7 @@ export const fetchMovies = async () => {
   };
 
   // Récupérer les films de plusieurs pages
-  const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]; // Changer le nombre de pages pour obtenir plus ou moins de films
+  const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]; // Changer le nombre de pages pour obtenir plus ou moins de films
   const movies = [];
   for (const page of pages) {
     const pageMovies = await fetchPage(page);
