878d13946bc4ca5c217663a17df1069e7a0e4906
sarahabl <sarahablonet.studio@gmail.com>
2024-07-22 15:12:59 +0200

    🌐 ajustement du nom des sliders / genre

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -4,25 +4,24 @@ import Slider from '../components/sliders/slider.jsx';
 import { fetchMovies } from '../api/tmdb-api.js';
 
 const genreIds = {
-  d action: 28,
-  d adventure: 12,
-  d animation: 16,
-  de comédie: 35,
-  de crime: 80,
-  documentaireq: 99,
-  de drama: 18,
-  à regarder en famille : 10751,
-  fantastiques: 14,
-  historiques : 36,
-  d horreur: 27,
-  musicaux : 10402,
-  de mystère: 9648,
-  de romance: 10749,
-  de science fiction: 878,
-  de télécrochet: 10770,
-  thriller: 53,
-  de guerre: 10752,
-  western : 37
+  "d'action": 28,
+  "d'adventure": 12,
+  "d'animation": 16,
+  "de comédie": 35,
+  "de crime": 80,
+  "documentaires": 99,
+  "de drama": 18,
+  "à regarder en famille": 10751,
+  "fantastiques": 14,
+  "historiques": 36,
+  "d'horreur": 27,
+  "musicaux": 10402,
+  "de romance": 10749,
+  "de science fiction": 878,
+  "de télécrochet": 10770,
+  "thriller": 53,
+  "de guerre": 10752,
+  "western": 37
 };
 
 const Home = () => {
