94a0f173d33742380640beb079012240dfed1b75
sarahabl <sarahablonet.studio@gmail.com>
2024-07-23 10:39:16 +0200

    💄 Changement image fond

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -8,12 +8,14 @@ const Hero = () => {
     const [error, setError] = useState(null);
     const [movies, setMovies] = useState([]);
     const [currentMovieIndex, setCurrentMovieIndex] = useState(0);
+    const [previousMovieIndex, setPreviousMovieIndex] = useState(null);
+    const [fadeClass, setFadeClass] = useState("fade-in");
 
     useEffect(() => {
         const getPopularMovies = async () => {
             try {
                 const popularMovies = await fetchMovies();
-                setMovies(popularMovies.slice(0, 10)); // Prendre les 10 premiers films populaires
+                setMovies(popularMovies.slice(0, 20)); // Prendre les 20 premiers films populaires
             } catch (error) {
                 console.error("Erreur lors de la récupération des films populaires:", error);
                 setError(error.message);
@@ -26,18 +28,24 @@ const Hero = () => {
     useEffect(() => {
         if (movies.length > 0) {
             const interval = setInterval(() => {
-                setCurrentMovieIndex(prevIndex => (prevIndex + 1) % movies.length);
-            }, 5000); // Changer de film toutes les 5 secondes
+                setFadeClass("fade-out");
+                setPreviousMovieIndex(currentMovieIndex);
+                setTimeout(() => {
+                    setCurrentMovieIndex(prevIndex => (prevIndex + 1) % movies.length);
+                    setFadeClass("fade-in");
+                }, 500); // Durée de l'animation de fondu
+            }, 3000); // Changer de film toutes les 3 secondes
 
             return () => clearInterval(interval);
         }
-    }, [movies]);
+    }, [movies, currentMovieIndex]);
 
     if (error) {
         return <div>Erreur: {error}</div>;
     }
 
     const currentMovie = movies[currentMovieIndex];
+    const previousMovie = previousMovieIndex !== null ? movies[previousMovieIndex] : null;
 
     return (
         <div className="position-relative">
@@ -46,18 +54,29 @@ const Hero = () => {
                 className="img-fluid w-100 banner-image"
                 alt="Le plus populaire"
             />
-            {currentMovie && currentMovie.backdrop_path && (
+            {previousMovie && previousMovie.backdrop_path && (
                 <div 
-                    className="position-absolute movie-backdrop"
+                    className={`position-absolute movie-backdrop fade-out`}
                     style={{
-                        backgroundImage: `url(https://image.tmdb.org/t/p/original${currentMovie.backdrop_path})`,
+                        backgroundImage: `url(https://image.tmdb.org/t/p/original${previousMovie.backdrop_path})`,
                         backgroundSize: 'cover',
                         backgroundPosition: 'center',
                         zIndex: 1
                     }}
                 />
             )}
-            <div className="position-absolute top-0 start-0 w-100 h-100 d-flex flex-column justify-content-center align-items-center" style={{ zIndex: 2 }}>
+            {currentMovie && currentMovie.backdrop_path && (
+                <div 
+                    className={`position-absolute movie-backdrop ${fadeClass}`}
+                    style={{
+                        backgroundImage: `url(https://image.tmdb.org/t/p/original${currentMovie.backdrop_path})`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        zIndex: 2
+                    }}
+                />
+            )}
+            <div className="position-absolute top-0 start-0 w-100 h-100 d-flex flex-column justify-content-center align-items-center" style={{ zIndex: 3 }}>
                 <div className="bg-overlay text-white p-4 w-100 titreBox">
                     <div className="tag mb-2 d-flex align-items-center">
                         <span className="me-2">🔥</span> Le + populaire
